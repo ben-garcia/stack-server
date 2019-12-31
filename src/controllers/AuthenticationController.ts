efe9b7bb4cfa056e1974b3462bf361639167fc28
= <7rubengarcia7@gmail.com>
2019-12-31 09:25:16 -0800

    [Update](AuthenticationController): added loginUser and test for it

--- src/controllers/AuthenticationController.ts
+++ src/controllers/AuthenticationController.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, Router } from 'express';
 import { getRepository, Repository } from 'typeorm';
 import Joi, { ObjectSchema } from '@hapi/joi';
+import bcrypt from 'bcrypt';
 
 import User from '../entity/User';
 import { Controller } from './types';
@@ -36,6 +37,7 @@ class AuthenticationController implements Controller {
 
   private initializeRoutes(): void {
     this.router.post('/register', this.registerUser);
+    this.router.post('/login', this.loginUser);
   }
 
   public registerUser = async (req: Request, res: Response) => {
@@ -62,6 +64,31 @@ class AuthenticationController implements Controller {
       res.status(201).json({ status: 'User Created' });
     } catch (e) {
       res.status(409).json({ error: e.detail });
+    }
+  };
+
+  public loginUser = async (req: Request, res: Response) => {
+    try {
+      const user = await this.userRepository.findOne({
+        email: req.body.email,
+      });
+
+      // if a user is found then compare password
+      if (!user) {
+        throw new Error('User with that email was not found');
+      } else {
+        const match = bcrypt.compare(req.body.password, user.password);
+        if (!match) {
+          throw new Error('passwords dont match');
+        }
+      }
+      res.status(200).json({ user });
+    } catch (e) {
+      // eslint-disable-next-line
+      console.log('Error: ', e);
+      res
+        .status(409)
+        .json({ error: 'No user with that email/password combination' });
     }
   };
 }
--- src/controllers/__tests__/AuthenticationController.test.ts
+++ src/controllers/__tests__/AuthenticationController.test.ts
@@ -3,18 +3,31 @@ import * as typeorm from 'typeorm';
 import AuthenticationController from '../AuthenticationController';
 
 describe('AuthenticationController', () => {
-  describe('/auth/register POST', () => {
-    // create the mock for 'getRepository' that gets called
-    // by the authenticationController constructor.
-    jest.spyOn(typeorm, 'getRepository').mockImplementation(
-      () =>
-        ({
-          create: jest.fn(),
-          save: jest.fn(),
-        } as any)
-    );
+  // create the mock for 'getRepository' that gets called
+  // by the authenticationController constructor.
+  jest.spyOn(typeorm, 'getRepository').mockImplementation(
+    () =>
+      ({
+        create: jest.fn(),
+        save: jest.fn(),
+      } as any)
+  );
+  const authenticationController = new AuthenticationController();
 
-    const authenticationController = new AuthenticationController();
+  describe('verify member variables', () => {
+    it('should have path equal to /auth', () => {
+      expect(authenticationController.path).toBe('/auth');
+    });
+
+    it('should have a router', () => {
+      expect(authenticationController.router).toBeDefined();
+    });
+
+    it('should have a userRepository', () => {
+      expect(authenticationController.userRepository).toBeDefined();
+    });
+  });
+  describe('/auth/register POST', () => {
     const mockRequest = {
       body: {
         email: 'test@email.com',
@@ -27,50 +40,58 @@ describe('AuthenticationController', () => {
       json: jest.fn(),
     };
 
-    describe('verify member variables', () => {
-      it('should have path equal to /auth', () => {
-        expect(authenticationController.path).toBe('/auth');
-      });
+    it('should call validateAsync with req.body', async () => {
+      authenticationController.schema.validateAsync = jest.fn();
 
-      it('should have a router', () => {
-        expect(authenticationController.router).toBeDefined();
-      });
+      await authenticationController.registerUser(
+        mockRequest as any,
+        mockResponse as any
+      );
 
-      it('should have a userRepository', () => {
-        expect(authenticationController.userRepository).toBeDefined();
-      });
+      expect(authenticationController.schema.validateAsync).toHaveBeenCalled();
+      expect(
+        authenticationController.schema.validateAsync
+      ).toHaveBeenCalledWith(mockRequest.body);
     });
 
-    describe('When attempting to register a new user', () => {
-      it('should call validateAsync with req.body', async () => {
-        authenticationController.schema.validateAsync = jest.fn();
+    it('should make sure registerUser method is called properlly', async () => {
+      authenticationController.registerUser = jest.fn();
+      await authenticationController.registerUser(
+        mockRequest as any,
+        mockResponse as any
+      );
 
-        await authenticationController.registerUser(
-          mockRequest as any,
-          mockResponse as any
-        );
+      expect(authenticationController.registerUser).toHaveBeenCalled();
+      expect(authenticationController.registerUser).toHaveBeenCalledWith(
+        mockRequest,
+        mockResponse
+      );
+    });
+  });
+  describe('/auth/login POST', () => {
+    const mockRequest = {
+      body: {
+        email: 'test2@email.com',
+        password: 'anotherpassword',
+      },
+    };
+    const mockResponse = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+    authenticationController.loginUser = jest.fn();
 
-        expect(
-          authenticationController.schema.validateAsync
-        ).toHaveBeenCalled();
-        expect(
-          authenticationController.schema.validateAsync
-        ).toHaveBeenCalledWith(mockRequest.body);
-      });
+    it('should call loginUser method with req.body', () => {
+      authenticationController.loginUser(
+        mockRequest as any,
+        mockResponse as any
+      );
 
-      it('should make sure registerUser method is called properlly', async () => {
-        authenticationController.registerUser = jest.fn();
-        await authenticationController.registerUser(
-          mockRequest as any,
-          mockResponse as any
-        );
-
-        expect(authenticationController.registerUser).toHaveBeenCalled();
-        expect(authenticationController.registerUser).toHaveBeenCalledWith(
-          mockRequest,
-          mockResponse
-        );
-      });
+      expect(authenticationController.loginUser).toHaveBeenCalled();
+      expect(authenticationController.loginUser).toHaveBeenCalledWith(
+        mockRequest,
+        mockResponse
+      );
     });
   });
 });
