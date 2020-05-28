4ce2967a99a34ac986fe29cf1115854cda14ad89
= <7rubengarcia7@gmail.com>
2020-05-28 08:45:33 -0700

    [Update](utils/createRedisClient): added 'error' event

--- src/utils/createRedisClient.ts
+++ src/utils/createRedisClient.ts
@@ -1,16 +1,27 @@
-import redis from 'redis';
+import redis, { RedisClient } from 'redis';
 
 const createRedisClient = () => {
   const isProduction = process.env.NODE_ENV === 'production';
+  let client: RedisClient;
   if (isProduction) {
-    return redis.createClient(process.env.REDIS_URL!);
+    // set heroku redis url in production
+    client = redis.createClient(process.env.REDIS_URL!);
+  } else {
+    // use localhost on development
+    client = redis.createClient({
+      host: '127.0.0.1',
+      port: 6379,
+      auth_pass: 'ben',
+    });
   }
 
-  return redis.createClient({
-    host: '127.0.0.1',
-    port: 6379,
-    auth_pass: 'ben',
+  // log error
+  client.on('error', error => {
+    // eslint-disable-next-line
+    console.error('createRedisClient error: ', error);
   });
+
+  return client;
 };
 
 export default createRedisClient;
