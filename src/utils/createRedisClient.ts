d8877721401d1f4aa7e6be01feb610d362f83858
= <7rubengarcia7@gmail.com>
2020-05-29 11:43:14 -0700

    [Update]: commented development redis options && logging the 'REDIS_URL' env variable

--- src/utils/createRedisClient.ts
+++ src/utils/createRedisClient.ts
@@ -1,17 +1,22 @@
 import redis, { RedisClient } from 'redis';
 
 const createRedisClient = () => {
-  let client: RedisClient;
-  if (process.env.REDIS_URL) {
-    client = redis.createClient(process.env.REDIS_URL);
-  } else {
-    // use localhost on development
-    client = redis.createClient({
-      host: '127.0.0.1',
-      port: 6379,
-      auth_pass: 'ben',
-    });
-  }
+  // eslint-disable-next-line
+  console.log('-----------------------------------');
+  // eslint-disable-next-line
+  console.log('REDIS_URL: ', process.env.REDIS_URL);
+  // eslint-disable-next-line
+  console.log('-----------------------------------');
+  // if (process.env.REDIS_URL) {
+  const client: RedisClient = redis.createClient(process.env.REDIS_URL!);
+  // } else {
+  // use localhost on development
+  //    client = redis.createClient({
+  //     host: '127.0.0.1',
+  //    port: 6379,
+  //   auth_pass: 'ben',
+  // });
+  // }
 
   // log error
   client.on('error', error => {
