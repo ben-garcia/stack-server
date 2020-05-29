40d96ef615db240ad448ff10fe5809d48faf34c8
= <7rubengarcia7@gmail.com>
2020-05-29 07:38:54 -0700

    [Update](createRedisClient): parsing REDIS_URL and passing host, password, port to 'createClient'

--- src/utils/createRedisClient.ts
+++ src/utils/createRedisClient.ts
@@ -1,13 +1,20 @@
 import redis, { RedisClient } from 'redis';
+import url from 'url';
 
 const createRedisClient = () => {
   let client: RedisClient;
   if (process.env.REDIS_URL) {
-    client = redis.createClient(process.env.REDIS_URL);
+    const parsedUrl = url.parse(process.env.REDIS_URL);
+    const password = parsedUrl.auth?.split(':')[1];
+    client = redis.createClient({
+      host: parsedUrl.host!,
+      password,
+      port: Number(parsedUrl.port),
+    });
   } else {
     // use localhost on development
     client = redis.createClient({
-      host: '127.0.0.1',
+      host: '127.0.0.11',
       port: 6379,
       auth_pass: 'ben',
     });
