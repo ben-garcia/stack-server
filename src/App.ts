870591ce6e4e2801f5d5c05930ddd955da9e7154
= <7rubengarcia7@gmail.com>
2021-06-30 09:30:20 -0700

    chore(App): add 'proxy' set to true again

--- src/App.ts
+++ src/App.ts
@@ -184,7 +184,7 @@ class App {
           secure: process.env.NODE_ENV === 'production', // set in production
         },
         name: 'stackSessionId', // name for the session id cookie
-        // proxy: true, // save session cookie id to client in Heroku
+        proxy: true, // save session cookie id to client in Heroku
         resave: false, // only save the session if it was modified during request
         saveUninitialized: false, // for login session
         secret: process.env.REDIS_SECRET ?? 'keyboard_cat',
